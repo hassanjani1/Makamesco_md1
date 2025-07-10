bf575eda9ca27f371b759f1bbc2111e83c9fe95c
JEEPERS CREEPER-XMD <Alonetech111@gmail.com>
2025-07-10 15:56:34 +0300

    Update repos.js

--- plugins/repos.js
+++ plugins/repos.js
@@ -1,78 +1,114 @@
-const axios = require("axios");
-const moment = require("moment-timezone");
+'use strict';
+
 const {
   ezra
-} = require(__dirname + "/../fredi/ezra");
-let dynamicForks = 5000;
-const fetchGitHubRepoDetails = async () => {
+} = require("../fredi/ezra");
+const axios = require("axios");
+const moment = require("moment-timezone");
+const set = require(__dirname + "/../set");
+moment.tz.setDefault('' + set.TIMEZONE);
+ezra({
+  'nomCom': "ping",
+  'categorie': "General-Fredi"
+}, async (_0x72e714, _0x36286d, _0x2f6f18) => {
+  let {
+    ms: _0x2bb02d
+  } = _0x2f6f18;
+  const {
+    time: _0x4e1d22,
+    date: _0x1b450d
+  } = {
+    'time': moment().format("HH:mm:ss"),
+    'date': moment().format("DD/MM/YYYY")
+  };
+  const _0x21013e = Math.floor(Math.random() * 100) + 1;
   try {
-    const _0x1c6838 = await axios.get("https://api.github.com/repos/mesh-matheka/Makamesco_md");
-    const {
-      name: _0x4ae93b,
-      stargazers_count: _0x27ef27,
-      watchers_count: _0x2237c0,
-      open_issues_count: _0x5424db,
-      forks_count: _0x4c9398,
-      owner: _0x38cd9a
-    } = _0x1c6838.data;
-    dynamicForks += _0x4c9398;
-    return {
-      'name': _0x4ae93b,
-      'stars': _0x27ef27,
-      'watchers': _0x2237c0,
-      'issues': _0x5424db,
-      'forks': dynamicForks,
-      'owner': _0x38cd9a.login,
-      'url': _0x1c6838.data.html_url
-    };
-  } catch (_0x5d335a) {
-    console.error("Error fetching GitHub repository details:", _0x5d335a);
-    return null;
-  }
-};
-const commands = ["git", "repo", "mchuga", "frezra", "ezrafre", "last", "liverpool", "anfield", "script", 'cs'];
-commands.forEach(_0x30efd7 => {
-  ezra({
-    'nomCom': _0x30efd7,
-    'categorie': "GitHub"
-  }, async (_0x3247d3, _0x23108e, _0x3c706d) => {
-    let {
-      repondre: _0xb6c54d
-    } = _0x3c706d;
-    const _0xec02f3 = await fetchGitHubRepoDetails();
-    if (!_0xec02f3) {
-      _0xb6c54d("âŒ Failed to fetch GitHub repository information.");
-      return;
-    }
-    const {
-      name: _0x2f3ef9,
-      stars: _0x104bd8,
-      watchers: _0x517909,
-      issues: _0x571494,
-      forks: _0x83a01e,
-      owner: _0x1b991d,
-      url: _0x35610a
-    } = _0xec02f3;
-    const _0x203945 = moment().tz("Africa/Dodoma").format("DD/MM/YYYY HH:mm:ss");
-    const _0x1cd310 = "\n😉 *" + _0x2f3ef9 + " REPO INFO* 😋\n\n👊 *Name:* " + _0x2f3ef9 + "\n🎖️ *Stars:* " + _0x104bd8.toLocaleString() + "\n🥂 *Forks:* " + _0x83a01e.toLocaleString() + "\n👁️ *Watchers:* " + _0x517909.toLocaleString() + "\n‼️ *Open Issues:* " + _0x571494.toLocaleString() + "\n🧑‍💻 *Owner:* " + _0x1b991d + "\n\n⌚ *Fetched on:* " + _0x203945 + "\n\n🔗 *Repo Link:* " + _0x35610a + "\n\n⚒️ Created By *Makmesco*\n\nStay using and follow my updates!";
-    try {
-      await _0x23108e.sendMessage(_0x3247d3, {
-        'text': _0x1cd310,
-        'contextInfo': {
-          'externalAdReply': {
-            'title': "🤝 Stay Updated with Makamesco Tech",
-            'body': "Tap here for the latest updates!",
-            'thumbnailUrl': "https://files.catbox.moe/sigghy.jpg",
-            'mediaType': 0x1,
-            'renderLargerThumbnail': true,
-            'mediaUrl': "https://whatsapp.com/channel/0029VbAEL9r5vKA7RCdnYG0S",
-            'sourceUrl': "https://whatsapp.com/channel/0029VbAEL9r5vKA7RCdnYG0S"
-          }
+    await _0x36286d.sendMessage(_0x72e714, {
+      'audio': {
+        'url': "https://files.catbox.moe/6xmof1.mp3"
+      },
+      'mimetype': "audio/mp4",
+      'ptt': true,
+      'contextInfo': {
+        'isForwarded': true,
+        'forwardedNewsletterMessageInfo': {
+          'newsletterJid': "120363366284524544@newsletter",
+          'newsletterName': "toxic lover",
+          'serverMessageId': 0x8f
+        },
+        'forwardingScore': 0x3e7,
+        'externalAdReply': {
+          'title': "toxic lover",
+          'body': "⚫ Pong: " + _0x21013e + "ms\n📅 *Date:* " + _0x1b450d + "\n⏰ *Time:* " + _0x4e1d22,
+          'thumbnailUrl': "https://files.catbox.moe/a4q16k.jpg",
+          'mediaType': 0x1,
+          'renderSmallThumbnail': true
         }
-      });
-    } catch (_0x2ec752) {
-      console.error("âŒ Error sending GitHub info:", _0x2ec752);
-      _0xb6c54d("âŒ Error sending GitHub info: " + _0x2ec752.message);
-    }
-  });
+      }
+    }, {
+      'quoted': _0x2bb02d
+    });
+  } catch (_0x5c09a2) {
+    console.log("❌ Ping Command Error: " + _0x5c09a2);
+    repondre("❌ Error: " + _0x5c09a2);
+  }
+});
+ezra({
+  'nomCom': "repo",
+  'categorie': "General-Fredi",
+  'reaction': '🫧',
+  'nomFichier': __filename
+}, async (_0x1e08fe, _0x16c249, _0x544c94) => {
+  const {
+    pushname: _0x48eea6,
+    repondre: _0x1843be
+  } = _0x544c94;
+  try {
+    const _0x273ae5 = await axios.get("https://github.com/toxiclover-tech/TOXIC-LOVER-MD");
+    const _0x540861 = _0x273ae5.data;
+    const _0x5abda2 = moment(_0x540861.updated_at).format("DD/MM/YYYY");
+    const _0xb0dc07 = "\nThis is\n\n> JEEPERS CREEPER-XMD\n\n*Don't forget fork and star repo*\n\n> *sᴛᴀʀs:* " + _0x540861.stargazers_count + "\n> *Forks:* " + _0x540861.forks_count + "\n> *Watchera:* " + _0x540861.watchers + "\n> *Updated:* " + _0x5abda2 + "\n> *Repo Link:*" + _0x540861.html_url + "*\n\n®";
+    await _0x16c249.sendMessage(_0x1e08fe, {
+      'image': {
+        'url': "https://files.catbox.moe/a4q16k.jpg"
+      },
+      'caption': _0xb0dc07,
+      'contextInfo': {
+        'isForwarded': true,
+        'forwardedNewsletterMessageInfo': {
+          'newsletterJid': "120363366284524544@newsletter",
+          'newsletterName': "toxic lover",
+          'serverMessageId': 0x8f
+        },
+        'forwardingScore': 0x3e7,
+        'externalAdReply': {
+          'title': "JEEPERS CREEPER-XMD",
+          'body': "🫧 repo link request 🫧",
+          'thumbnailUrl': "https://files.catbox.moe/a4q16k.jpg",
+          'mediaType': 0x1,
+          'mediaUrl': '',
+          'sourceUrl': ''
+        }
+      }
+    });
+    await _0x16c249.sendMessage(_0x1e08fe, {
+      'audio': {
+        'url': "https://files.catbox.moe/6xmof1.mp3"
+      },
+      'mimetype': "audio/mp4",
+      'ptt': true,
+      'caption': "*🫧 JEEPERS CREEPER-XMD repo song 🫧",
+      'contextInfo': {
+        'isForwarded': true,
+        'forwardedNewsletterMessageInfo': {
+          'newsletterJid': "120363366284524544@newsletter",
+          'newsletterName': "toxic lover",
+          'serverMessageId': -1
+        }
+      }
+    });
+  } catch (_0x24b701) {
+    console.log("Error fetching data:", error);
+    _0x1843be("❌ Error fetching repository data. Please try again later.");
+  }
 });
