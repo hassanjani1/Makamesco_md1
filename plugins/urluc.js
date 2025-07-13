13bae604dde1d1e5ce80398626983ba96e401f7d
mesh-matheka <sescotech70@gmail.com>
2025-07-13 20:08:31 +0300

    Update urluc.js

--- plugins/urluc.js
+++ plugins/urluc.js
@@ -88,13 +88,13 @@ ezra({ nomCom: "url", categorie: "General", reaction: "👨🏿‍💻" }, async
         // Respond with the URL based on media type
         switch (mediaType) {
             case 'image':
-                repondre(`Lucky url: ${catboxUrl}`);
+                repondre(`makamesco url: ${catboxUrl}`);
                 break;
             case 'video':
-                repondre(`lucky url: ${catboxUrl}`);
+                repondre(`makamesco url: ${catboxUrl}`);
                 break;
             case 'audio':
-                repondre(`lucky url: ${catboxUrl}`);
+                repondre(`makamesco url: ${catboxUrl}`);
                 break;
             default:
                 repondre('An unknown error occurred.');
